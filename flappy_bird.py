8e4c8012c5772d913d8487946e3ea5926a4e53b9
Yaro <jaroba0@gmail.com>
2023-04-24 19:49:52 +0200

    dokoncene 3. video , animacia , gravitacia , ked spadne na zem - funguje

--- flappy_bird.py
+++ flappy_bird.py
@@ -11,7 +11,7 @@ fps = 60
 
 #nastavovanie sirky a dlzky ptm nastavenie na screen + nazov 
 screen_width = 864
-screen_height = 936
+screen_height = 836
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
@@ -19,12 +19,18 @@ pygame.display.set_caption("Flappy Bird")
 #definovanie somarin
 ground_scroll = 0
 scroll_speed = 4
+flying = False
+game_over = False
 
 #nacitavanie obrazkov , musite mat img subor v tom obrazky 
 bg = pygame.image.load("img/etika.webp")
-ground = pygame.image.load("img/ground.png")
+ground_img = pygame.image.load("img/ground.png")
 
 #zapinanie cez terminal - prave tlacitko - run python file in terminal 
+
+
+
+
 
 #sprite class animacia postavy , images 
 class Bird(pygame.sprite.Sprite):
@@ -42,25 +48,65 @@ class Bird(pygame.sprite.Sprite):
         self.image = self.images[self.index]
         self.rect  = self.image.get_rect()
         self.rect.center = [x,y]
+        self.vel = 0
+        self.clicked = False
 
     def update(self):
-        # daco s animacia
-        self.counter += 1 
-        flap_cooldown = 5
+        #nastavovanie gravity
+        if flying == True:
+            self.vel += 0.5
+            if self.vel >8:
+                self.vel = 8
+            print(self.vel)
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
 
-        if self.counter > flap_cooldown:
-            self.counter = 0
-            self.index += 1 
-            if self.index >= len(self.images):
-                self.index = 0
 
-        self.image = self.images[self.index]
+
+        if game_over == False:
+            #jumping
+            if pygame.mouse.get_pressed()[0]==1 and self.clicked == False:
+                self.clicked = True
+                self.vel = -10
+            #nastavovanie aby pri drzani mysi fľepi neskakal
+            if pygame.mouse.get_pressed()[0]==0:
+                self.clicked = False
+
+            # cooldown na obrazky -animacia
+            self.counter += 1 
+            flap_cooldown = 5
+
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1 
+                if self.index >= len(self.images):
+                    self.index = 0
+
+            self.image = self.images[self.index]
+
+
+
+                #rotating bird po skakani , 
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -1)
+        else:
+             self.image = pygame.transform.rotate(self.images[self.index],-90)
+
+
+
+
+
+
+
 
 
 #nastavenie kde bude flappy zacinat
 bird_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
+
+
+
+
 
 
 
@@ -76,17 +122,28 @@ while run:
     bird_group.draw(screen)
     bird_group.update()
 
+    #nakresli ground
+    screen.blit(ground_img, (ground_scroll , 768))
+
+    #ked fľepi hitne ground
+    if flappy.rect.bottom > 768:
+        game_over = True
+        flying = False
+
+    if game_over == False:
+
     #scrollovanie ground
-    screen.blit(ground, (ground_scroll,768))
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
+        ground_scroll -= scroll_speed
+        if abs(ground_scroll) > 35:
+            ground_scroll = 0
     
 
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
 
     pygame.display.update()
 #cau pygame
